67aeea12ca7598ede357db6757d6ed9f0a725535
DataScientist-CL <DSprime.cl@gmail.com>
2025-07-06 16:20:25 +0900

    list_voices.py

--- list_voices.py
+++ list_voices.py
@@ -2,7 +2,7 @@
 from elevenlabs.client import ElevenLabs
 
 # 1) 환경변수 대신 직접 키를 하드코딩
-client = ElevenLabs(api_key="sk_7f5157e3203ee6318d3678898ac7a01f046c695752e636b3")
+client = ElevenLabs(api_key="YOUR_API_KEY")
 
 # 2) ElevenLabs v2 SDK에서 제공하는 메서드로 목소리 목록 가져오기
 response = client.voices.search()
